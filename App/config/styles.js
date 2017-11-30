ddc13b3a744ad0ddc7089c40c15bc7ab0173ef78
alxchen <alison0716@gmail.com>
2017-11-30 14:51:34 -0800

    Minor update: removed some images

--- App/config/styles.js
+++ App/config/styles.js
@@ -1,10 +1,57 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
+
+const win = Dimensions.get('window');
 
 export default styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 30,
+        textAlign: 'center',
+        margin: 5
+    },
+    image: {
+        alignSelf: 'stretch',
+        width: win.width,
+        height: win.height
+    },
+    bannerTop: {
+      color: 'white',
+        fontSize: 18,
+        marginTop: 10,
+        marginBottom: 5,
+        marginLeft: 10,
+        fontWeight: 'bold',
+    },
+    bannerBottom: {
+        color: 'white',
+        fontSize: 18,
+        marginTop: 5,
+        marginBottom: 10,
+        marginLeft: 10
+    },
+    margin: {
+        margin: 20
+    },
+    profileBar: {
+        paddingLeft: 20,
+        width: win.width,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    },
+    baseText: {
+        fontFamily: 'Avenir',
+    },
+    curatorText: {
+        padding: 10,
+        fontSize: 18
+    },
+
 });
